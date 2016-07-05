cdf292e932aa8f91cfe8e622d1527c1a143738cb
maifei <kehao.qiu@gmail.com>
2016-07-05 15:31:55 +0800

    modified:   postcss/test.js

--- postcss/test.js
+++ postcss/test.js
@@ -1,6 +1,40 @@
 var postcss = require('postcss');
 var webpcss = require('webpcss');
 var r = postcss([webpcss.default])
-    .process('.fulishe{width:10rem;background-image:url(../images/chenggou-bg.png)} .fulishe2{background-image:url(../images/homepage.jpg)}')
-    .then(function(r){console.log(r.css)}, function(err) {});
+    .process('@charset "UTF-8";'
+            + '.test1{_width:10rem;background-image:url(../images/chenggou-bg.png)}'
+            + '.test2{background-image:url(../images/homepage.jpg)}'
+            )
+   
+// console.log(r);
+// console.log(r.result);
+// console.log(r.result.root);
+// console.log(r.result.root.last);
+// console.log(r.result.root.first);
+
+var root = r.result.root;
+var rule = root.nodes[1];
+// console.log(rule);
+
+// const decl2 = postcss.decl({ prop: 'background-color', value: 'white'  });
+// new rule
+// const newRule = postcss.parse('.test3{}').first;
+
+//append
+// const decl1 = postcss.decl({ prop: 'color', value: 'black'  });
+// newRule.append(decl1);
+
+
+//cloneBefore
+// var cloneDecl = decl1.cloneBefore({ prop: '-webkit-' + decl1.prop  });
+// // console.log(cloneDecl);
+// //moveTo
+// var toParent = root.nodes[2]
+// // console.log(toParent);
+// cloneDecl.moveTo(toParent);
+
+// root.append(newRule);
+
+// r.then(function(r){console.log(r.css)}, function(err) {});
+
 
